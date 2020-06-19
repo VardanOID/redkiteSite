6220921c254eeaffb4ca1c8e6cd7843501d5cdb4
Vardan Hambardzumyan <vhambardzumyan@carsondellosa.com>
2020-06-19 16:50:39 +0400

    Updates

---
+++ precache-manifest.7ab9ceef44dd5b9df3cae7ced45e19e8.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6ea09b5c72965e2eb494e3ed6ac8c9",
+    "url": "/redkiteSite/index.html"
+  },
+  {
+    "revision": "f204e21c47fb5d4cb60d",
+    "url": "/redkiteSite/static/css/2.86abc55e.chunk.css"
+  },
+  {
+    "revision": "b94198da1413c579b92a",
+    "url": "/redkiteSite/static/css/main.a7d97a89.chunk.css"
+  },
+  {
+    "revision": "f204e21c47fb5d4cb60d",
+    "url": "/redkiteSite/static/js/2.78176809.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/redkiteSite/static/js/2.78176809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94198da1413c579b92a",
+    "url": "/redkiteSite/static/js/main.79a71eae.chunk.js"
+  },
+  {
+    "revision": "0d97046e0184b397c5bc",
+    "url": "/redkiteSite/static/js/runtime-main.2c7330e9.js"
+  },
+  {
+    "revision": "74e3aba834541454f364bf8cc89a79f8",
+    "url": "/redkiteSite/static/media/10@2x.74e3aba8.png"
+  },
+  {
+    "revision": "4ad8f6783f3a105ac589c1293ce4a451",
+    "url": "/redkiteSite/static/media/1@2x.4ad8f678.png"
+  },
+  {
+    "revision": "ecfb2891f3af2332366ea56945a509e3",
+    "url": "/redkiteSite/static/media/2@2x.ecfb2891.png"
+  },
+  {
+    "revision": "22b61ade8131324905f275eeecefd2ba",
+    "url": "/redkiteSite/static/media/3@2x.22b61ade.png"
+  },
+  {
+    "revision": "b0bf0bc32ed8ce5b57526c92228db8b0",
+    "url": "/redkiteSite/static/media/4@2x.b0bf0bc3.png"
+  },
+  {
+    "revision": "7e60c0be89bf0a184ded1d3116c3733a",
+    "url": "/redkiteSite/static/media/5@2x.7e60c0be.png"
+  },
+  {
+    "revision": "879eb1065e70f9e22a2fa540088b2b79",
+    "url": "/redkiteSite/static/media/6@2x.879eb106.png"
+  },
+  {
+    "revision": "bc4f6c174d73fcc1e1d2cb7752311ade",
+    "url": "/redkiteSite/static/media/7@2x.bc4f6c17.png"
+  },
+  {
+    "revision": "f0b725c8692285c9e2b22b659141fb62",
+    "url": "/redkiteSite/static/media/8@2x.f0b725c8.png"
+  },
+  {
+    "revision": "37561eea15646c458c03fa37ad27bd40",
+    "url": "/redkiteSite/static/media/9@2x.37561eea.png"
+  },
+  {
+    "revision": "3c740efe9693370c7160dc6e8141e840",
+    "url": "/redkiteSite/static/media/BgVideo_Pic.3c740efe.png"
+  },
+  {
+    "revision": "17bdbf8048841a5d135881d361e3cd91",
+    "url": "/redkiteSite/static/media/Bg_image.17bdbf80.png"
+  },
+  {
+    "revision": "107a18d26a3ba447c865862ec48220f1",
+    "url": "/redkiteSite/static/media/Facebook.107a18d2.svg"
+  },
+  {
+    "revision": "fe865f877f86bbf3ae3f60925947a0dc",
+    "url": "/redkiteSite/static/media/IconCall.fe865f87.svg"
+  },
+  {
+    "revision": "66784d53ceb9ee1c31a005f9836d4adf",
+    "url": "/redkiteSite/static/media/IconCycle.66784d53.svg"
+  },
+  {
+    "revision": "24ea216dbaceb104f0457ef81c7097a2",
+    "url": "/redkiteSite/static/media/IconGrowth.24ea216d.svg"
+  },
+  {
+    "revision": "bc64d1486a2b866b9f3f9954c5bf287f",
+    "url": "/redkiteSite/static/media/IconLeft.bc64d148.svg"
+  },
+  {
+    "revision": "988e189b2d2859fcad5f79b949b3f677",
+    "url": "/redkiteSite/static/media/IconMail.988e189b.svg"
+  },
+  {
+    "revision": "feb0868bfa8d4fbd0fd926d0cfe7e3d3",
+    "url": "/redkiteSite/static/media/IconPersonal.feb0868b.svg"
+  },
+  {
+    "revision": "0ac1c120f35ed0ddcf05f382758d7c77",
+    "url": "/redkiteSite/static/media/IconPlace.0ac1c120.svg"
+  },
+  {
+    "revision": "1daa234792605255670f5db7c54409d7",
+    "url": "/redkiteSite/static/media/IconQuality.1daa2347.svg"
+  },
+  {
+    "revision": "9a589378d30b854403b0c85e8bfa2923",
+    "url": "/redkiteSite/static/media/IconRight.9a589378.svg"
+  },
+  {
+    "revision": "76128bb64dae600d3dc9adcb1ed3fd18",
+    "url": "/redkiteSite/static/media/Icon_Phone.76128bb6.svg"
+  },
+  {
+    "revision": "f3b51d4c5b9c71a97f58fec7a83d7b51",
+    "url": "/redkiteSite/static/media/Icon_Play.f3b51d4c.svg"
+  },
+  {
+    "revision": "ac6760c1b4857e2a565b4c5ee6b0b4eb",
+    "url": "/redkiteSite/static/media/Instagram.ac6760c1.svg"
+  },
+  {
+    "revision": "333c28a1043f3fcc217bfe04fd25214b",
+    "url": "/redkiteSite/static/media/Logo.333c28a1.svg"
+  },
+  {
+    "revision": "b53b06d58a937ef8f110f2a8bfb48295",
+    "url": "/redkiteSite/static/media/LogoWhite.b53b06d5.svg"
+  },
+  {
+    "revision": "625f43f23fd77eeff58108731f710e19",
+    "url": "/redkiteSite/static/media/OurVision.625f43f2.svg"
+  },
+  {
+    "revision": "88731eda4513a75eaa7268c2f9c90cb7",
+    "url": "/redkiteSite/static/media/TheTeam.88731eda.svg"
+  },
+  {
+    "revision": "744acefdfbab25532496cfd06d2c234a",
+    "url": "/redkiteSite/static/media/a.744acefd.png"
+  },
+  {
+    "revision": "cecc309adce2deeacd5bdb821ce17495",
+    "url": "/redkiteSite/static/media/b.cecc309a.png"
+  },
+  {
+    "revision": "4904400d4fc9fd288e4828642bc9cf3d",
+    "url": "/redkiteSite/static/media/c.4904400d.png"
+  },
+  {
+    "revision": "23b08a629b29cb00ce67226fd68762ef",
+    "url": "/redkiteSite/static/media/d.23b08a62.png"
+  },
+  {
+    "revision": "3ee47d9ae0f3c2dfba2f2511b3907e84",
+    "url": "/redkiteSite/static/media/work1.3ee47d9a.png"
+  },
+  {
+    "revision": "36f21aa73d13d5abfc1e9561ac65725a",
+    "url": "/redkiteSite/static/media/work2.36f21aa7.png"
+  },
+  {
+    "revision": "9c67693975334c4fbdaadbe00c166d0f",
+    "url": "/redkiteSite/static/media/work3.9c676939.png"
+  },
+  {
+    "revision": "383ab089594d5c2f79e8f77a74db1bb9",
+    "url": "/redkiteSite/static/media/work4.383ab089.png"
+  }
+]);
